b842d46012620d3cdc520dc28a966bfb2f4d7992
DEEPNAPOOja <poojathmideepna@gmail.com>
2024-01-18 17:55:21 +0530

    Add project files.

---
+++ HRM_System/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM_System
+{
+    public class SD
+    {
+        public const String Role_Admin = "Admin";
+        public const String Role_Employee = "Employee";
+    }
+}
